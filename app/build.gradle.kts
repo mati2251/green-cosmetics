06287dc0265f578aebb6a4586e8aca91a3a8bef3
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2024-05-20 21:36:14 +0200

    Added barcode camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -70,9 +72,12 @@ dependencies {
     implementation("androidx.compose.material:material-icons-core")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3-window-size-class")
-
+    implementation("com.google.accompanist:accompanist-permissions:0.35.1-alpha")
+    implementation("androidx.camera:camera-camera2:1.2.2")
+    implementation("com.google.mlkit:barcode-scanning:17.1.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.compose.runtime:runtime-livedata")
+
 
 }
--- app/src/main/java/pl/put/greencosmetics/pages/Main.kt
+++ app/src/main/java/pl/put/greencosmetics/pages/Main.kt
@@ -1,14 +1,111 @@
 package pl.put.greencosmetics.pages
 
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun Main(){
-    Text(text = "Main")
+fun Main() {
+    val state = rememberPermissionState(android.Manifest.permission.CAMERA)
+
+    if (state.status.isGranted) {
+        CameraScreen()
+    } else if (state.status.shouldShowRationale) {
+        Text("Camera Permission permanently denied")
+    } else {
+        SideEffect {
+            state.run { launchPermissionRequest() }
+        }
+        Text("No Camera Permission")
+    }
 }
 
 @Composable
 fun CameraScreen() {
+    val localContext = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val cameraProviderFuture = remember {
+        ProcessCameraProvider.getInstance(localContext)
+    }
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            val previewView = PreviewView(context)
+            val preview = Preview.Builder().build()
+            val selector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
 
+            preview.setSurfaceProvider(previewView.surfaceProvider)
+            val imageAnalysis = ImageAnalysis.Builder().build()
+            imageAnalysis.setAnalyzer (ContextCompat.getMainExecutor(context), BarcodeAnalyzer(context))
+            runCatching {
+                cameraProviderFuture.get().bindToLifecycle(
+                    lifecycleOwner,
+                    selector,
+                    preview,
+                    imageAnalysis
+                )
+            }.onFailure {
+                Log.e("CAMERA", "Camera bind error ${it.localizedMessage}", it)
+            }
+            previewView
+        }
+    )
+}
+
+class BarcodeAnalyzer(private val context: Context) : ImageAnalysis.Analyzer {
+
+    private val options = BarcodeScannerOptions.Builder()
+        .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
+        .build()
+
+    private val scanner = BarcodeScanning.getClient(options)
+
+    @androidx.annotation.OptIn(ExperimentalGetImage::class)
+    override fun analyze(imageProxy: ImageProxy) {
+        imageProxy.image
+            ?.let { image ->
+                scanner.process(
+                    InputImage.fromMediaImage(
+                        image, imageProxy.imageInfo.rotationDegrees
+                    )
+                ).addOnSuccessListener { barcode ->
+                    barcode?.takeIf { it.isNotEmpty() }
+                        ?.mapNotNull { it.rawValue }
+                        ?.joinToString(",")
+                        ?.let { barcodeValue ->
+                            Toast.makeText(context, barcodeValue, Toast.LENGTH_SHORT).show()
+                        }
+                }.addOnCompleteListener {
+                    imageProxy.close()
+                }
+            }
+    }
 }
